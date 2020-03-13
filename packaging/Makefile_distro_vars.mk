075b6eacdf409f9322733a2f8c477679056ee00f
John E Malmberg <john.e.malmberg@intel.com>
2020-03-13 09:09:55 -0500

    CORCI-881 build: Update per review comments
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

--- packaging/Makefile_distro_vars.mk
+++ packaging/Makefile_distro_vars.mk
@@ -70,3 +70,18 @@ define install_repo
 	zypper --non-interactive ar $(1)
 endef
 endif
+ifeq ($(ID_LIKE),debian)
+ifndef LANG
+export LANG = C.UTF-8
+endif
+ifndef LC_ALL
+export LC_ALL = C.UTF-8
+endif
+else
+ifndef LANG
+export LANG = en_US.UTF-8
+endif
+ifndef LC_ALL
+export LC_ALL = en_US.UTF-8
+endif
+endif
